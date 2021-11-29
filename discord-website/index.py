60e92239ce5d820720a8276a2982f6b8728863b1
xerio1001 <dariovanhasselt@hotmail.be>
2021-11-29 10:39:00 +0100

    comment

--- discord-website/index.py
+++ discord-website/index.py
@@ -1,4 +1,3 @@
-from logging import debug
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +6,14 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("index.html")
-    
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/logged_in")
+def logged_in():
+    return render_template("logged_in.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
