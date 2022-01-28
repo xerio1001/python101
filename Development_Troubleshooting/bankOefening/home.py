09a4173009f12a05fcc6b5727c23a50962721b08
xerio1001 <dariovanhasselt@hotmail.be>
2022-01-28 11:20:58 +0100

    a

--- Development_Troubleshooting/bankOefening/home.py
+++ Development_Troubleshooting/bankOefening/home.py
@@ -36,15 +36,21 @@ class Rekeningen:
         return NotImplemented
 
 class Zichtrekening (Rekeningen):
-    #def __init__(self, voornaam, naam, rijkregnum, rekeningnummer, beginOverzicht):
-        #super().__init__(voornaam, naam, rijkregnum, rekeningnummer)
-        #self.overzicht = beginOverzicht
+    def __init__(self, rekeningnummer, beginOverzicht, spaarnum):
+        super().__init__(rekeningnummer)
+        self.overzicht = beginOverzicht
+        self.spaarnum = spaarnum
 
     def afhalen(self, bedrag):
         self.overzicht = self.overzicht - bedrag
 
     def overschrijven(self, bedrag):
-        self.overzicht = self.overzicht - bedrag
+        if(self.overzicht - bedrag > 0 and self.spaarnum == ""):
+            self.storten(bedrag)
+            self.overzicht = self.overzicht - bedrag
+        if(self.overzicht - bedrag > 0 and self.spaarnum != ""):
+            self.spaarnum.storten(bedrag)
+            self.overzicht = self.overzicht - bedrag
 
 class Spaarrekening (Rekeningen):
     def __init__(self, rekeningnummer, beginOverzicht, zichtnum):
@@ -64,6 +70,8 @@ persoon1 = Persoon("Dario", "Van Hasselt", "02.01.08-892.05")
 gegevens1 = Zichtrekening("091-0122401-16")
 gegevens2 = Spaarrekening("000-0000097-97", 1000, gegevens1)
 gegevens2.overschrijven(250)
+gegevens1.afhalen(100)
+gegevens1 = Zichtrekening("091-0122401-16", 1000, gegevens2)
 
 print("Zichtrekening ", gegevens1.overzicht)
 print("Spaarrekening ", gegevens2.overzicht)
