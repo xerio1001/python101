4928cc96d48809a33025cb38ad952e9eb4532529
xerio1001 <dariovanhasselt@hotmail.be>
2022-01-31 16:02:21 +0100

    a

--- Development_Troubleshooting/bankOefening/home.py
+++ Development_Troubleshooting/bankOefening/home.py
@@ -5,9 +5,10 @@ class Persoon:
         self.rijkregnum = rijkregnum
 
 class Rekeningen:
-    def __init__(self, rekeningnummer):
-        self.overzicht = 0
+    def __init__(self, rekeningnummer, saldo, persoon):
+        self.saldo = saldo
         self.rekeningnummer = rekeningnummer
+        self.persoon = persoon
 
     def controleReknum(self):
         self.rekeningnummer = self.rekeningnummer.replace(".","").replace("/","").replace("-","").replace(" ","")
@@ -29,49 +30,59 @@ class Rekeningen:
         else:
             return "Ongeldige lengte van het nummer."
 
-    def storten(self, bedrag):
-        self.overzicht = self.overzicht + bedrag
-
     def overschrijven(self):
         return NotImplemented
 
+
+    
 class Zichtrekening (Rekeningen):
-    def __init__(self, rekeningnummer, beginOverzicht, spaarnum):
-        super().__init__(rekeningnummer)
-        self.overzicht = beginOverzicht
-        self.spaarnum = spaarnum
+        def storten(self, bedrag):
+            self.saldo = self.saldo + bedrag
 
-    def afhalen(self, bedrag):
-        self.overzicht = self.overzicht - bedrag
+        def afhalen(self, bedrag):
+            if(bedrag > self.saldo):
+                print("Het gegeven bedrag is te groot.")
+            else:
+                self.saldo = self.saldo - bedrag
 
-    def overschrijven(self, bedrag):
-        if(self.overzicht - bedrag > 0 and self.spaarnum == ""):
-            self.storten(bedrag)
-            self.overzicht = self.overzicht - bedrag
-        if(self.overzicht - bedrag > 0 and self.spaarnum != ""):
-            self.spaarnum.storten(bedrag)
-            self.overzicht = self.overzicht - bedrag
+        def overschrijven(self):
+            pass
+
+
 
 class Spaarrekening (Rekeningen):
-    def __init__(self, rekeningnummer, beginOverzicht, zichtnum):
-        super().__init__(rekeningnummer)
-        self.overzicht = beginOverzicht
-        self.zichtnum = zichtnum
-
-    def overschrijven(self, bedrag):
-        if(self.overzicht - bedrag > 0):
-            self.zichtnum.storten(bedrag)
-            self.overzicht = self.overzicht - bedrag
+    def __init__(self, rekeningnummer, saldo, persoon, zicht):
+        super().__init__(self, rekeningnummer, saldo, persoon)
+        if isinstance(zicht, Zichtrekening):
+            self.zicht = zicht
         else:
-            return print("Er is een fout opgetreden bij het overschrijven van de spaarrekening!")
+            print("Dit is geen geldige zichtrekening")
 
+    def overschrijven(self, bedrag, zicht):
+        if(bedrag > self.saldo):
+            print("Gaat niet.")
+
+        elif(self.zicht == zicht):
+            if isinstance(zicht, Zichtrekening):
+                self.saldo = self.saldo - bedrag
+                zicht.storten(bedrag)
+            else:
+                print("Dit is geen geldige zichtrekening")
+
+        else:
+            print("Ongeldige rekeningnummer")
 
 persoon1 = Persoon("Dario", "Van Hasselt", "02.01.08-892.05")
-gegevens1 = Zichtrekening("091-0122401-16", 1000, "")
-gegevens2 = Spaarrekening("000-0000097-97", 1000, gegevens1)
-gegevens2.overschrijven(250)
-gegevens1.afhalen(100)
+zicht1 = Zichtrekening("091-0122401-16", 1000, persoon1)
+spaar1 = Spaarrekening("000-0000097-97", 2000, persoon1, zicht1)
+
+spaar1.overschrijven(100, zicht1)
+zicht1.storten(500)
+zicht1.afhalen(400)
+
+print(zicht1.saldo)
+print(spaar1.saldo)
 
 
-print("Zichtrekening ", gegevens1.overzicht)
-print("Spaarrekening ", gegevens2.overzicht)
+#Op de zichtrekening kunnen we geld storten, geld afhalen, en geld overschrijven naar andere rekeningen, ook naar onze eigen spaarrekening.
+#Op de spaarrekening kunnen we enkel geld terug overschrijven naar de gekoppelde zichtrekening. Opgelet: spaarrekening kan niet onder nul.
