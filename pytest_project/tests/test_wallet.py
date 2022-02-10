7d336ca5cff0bf1e214413ea02bab12ef4b1282c
xerio1001 <dariovanhasselt@hotmail.be>
2022-02-10 15:55:36 +0100

    a

--- pytest_project/tests/test_wallet.py
+++ pytest_project/tests/test_wallet.py
@@ -4,29 +4,36 @@ import pytest
 from pytest_project.wallet import Wallet, InsufficientAmount
 
 
-def test_default_initial_amount():
-    wallet = Wallet()
-    assert wallet.balance == 0
+@pytest.fixture
+def empty_wallet():
+    """Returns a Wallet instance with a zero balance"""
+    return Wallet()
 
 
-def test_setting_initial_amount():
-    wallet = Wallet(100)
+@pytest.fixture
+def wallet():
+    """Returns a Wallet instance with a balance of 20"""
+    return Wallet(20)
+
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+
+def test_setting_initial_amount(wallet):
+    assert wallet.balance == 20
+
+
+def test_wallet_add_cash(wallet):
+    wallet.add_cash(80)
     assert wallet.balance == 100
 
 
-def test_wallet_add_cash():
-    wallet = Wallet(10)
-    wallet.add_cash(90)
-    assert wallet.balance == 100
-
-
-def test_wallet_spend_cash():
-    wallet = Wallet(20)
+def test_wallet_spend_cash(wallet):
     wallet.spend_cash(10)
     assert wallet.balance == 10
 
 
-def test_wallet_spend_cash_raises_exception_on_insufficient_amount():
-    wallet = Wallet()
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     with pytest.raises(InsufficientAmount):
-        wallet.spend_cash(100)
+        empty_wallet.spend_cash(100)
