de52a1418357775e84fa4eb903140f1b6b04bcda
xerio1001 <dariovanhasselt@hotmail.be>
2022-02-21 14:34:05 +0100

    a

--- test_develoment_dotenv/test_develoment_dotenv/main.py
+++ test_develoment_dotenv/test_develoment_dotenv/main.py
@@ -3,4 +3,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.environ.get("API_KEY")
+uri = os.environ.get("DB_URI")
+port = os.environ.get("DB_PORT")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+msg = os.environ.get("HELLO_MSG")
+
 print(token)
+print(uri)
+print(port)
+print(user)
+print(password)
+print(msg)
