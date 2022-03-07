e92d2430160eb55556c4c5fef7f05108676c2618
xerio1001 <dariovanhasselt@hotmail.be>
2022-03-07 16:52:03 +0100

    webapp

--- flask_test1/flask_test1/main.py
+++ flask_test1/flask_test1/main.py
@@ -1,66 +1,28 @@
-from socket import getnameinfo
-from flask import Flask
+from turtle import title
+from flask import Flask, render_template
 
 # we maken een object met de naam app van de Flask module
 app = Flask(__name__)
 
 @app.route('/') # dit leidt onze webpagina naar de juiste functie
-def home():
-    return \
-        """
-        <body style='background-color: #ededed'>
-        <center>
-        <h2>Welkom!</h2> 
-        <p>
-        <a href='/data' style='font-size: 14pt; text-decoration: none;'>Naar data</a> 
-        </p>
-        <p>
-        <a href='/hobbies' style='font-size: 14pt; text-decoration: none;'>Naar hobbies</a>
-        </p>
-        </center>
-        """
+def home(titel=None):
+    return render_template('index.html', titel='Home Page')
 
 @app.route('/data')
-def data():
-    return \
-    """
-    <table align='center'>
-        <tr>
-            <td>Voornaam</td>
-            <td><input type='text' placeholder='Dario'></td>
-        </tr>
-        <tr>
-            <td>Naam:</td>
-            <td><input type='text' placeholder='Van Hasselt'></td>
-        </tr>
-        <tr>
-            <td>ouderdom:</td>
-            <td><input type='number' placeholder='20'></td>
-        </tr>
-        </tr>
-        <tr>
-            <td>Woonplaats:</td>
-            <td><input type='text' placeholder='Mol'></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><a href='/'>Naar home</a></td>
-        </tr>
-    </table>
-    """
-
+def data(titel=None):
+    return render_template('data.html', titel='Data')
 
 @app.route('/hobbies')
 def hobbies():
-    return \
-     """
-     <center>
-     Actieve hobbies zijn: gitaar/gamen 
-     <p> 
-     <a href='/'>Terug naar home</a>
-     </p>
-     </center>
-     """
+    return render_template('hobbies.html', titel='Hobbies')
+
+@app.route('/hobbies/gitaar')
+def gitaar():
+    return render_template('gitaar.html', titel='Gitaar')
+
+@app.route('/hobbies/gamen')
+def gamen():
+    return render_template('gamen.html', titel='Gamen')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
