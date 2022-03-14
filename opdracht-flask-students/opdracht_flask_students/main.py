00ec774bb57cb1b043eececffc54a6aa995f1f4d
xerio1001 <dariovanhasselt@hotmail.be>
2022-03-14 15:09:03 +0100

    a

--- opdracht-flask-students/opdracht_flask_students/main.py
+++ opdracht-flask-students/opdracht_flask_students/main.py
@@ -2,72 +2,84 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-students = [
-    {
-        "id": 0,
+students = {
+    0: {
         "Name": "Dario V.H.",
         "Age": 20,
         "Gender": "Male",
         "Phone": "0123456789",
-        "CourseID": 1
+        "CourseID": 1,
+        "Result": {
+            3: 90,
+            4: 70,
+            5: 80
+        }
     },
-    {
-        "id": 1,
+    1: {
         "Name": "Jens C.",
         "Age": 32,
         "Gender": "Male",
         "Phone": "9876543210",
-        "CourseID": 0
+        "CourseID": 0,
+        "Result": {
+            0: 80,
+            1: 80,
+            2: 70
+        }
     },
-    {
-        "id": 2,
+    2: {
         "Name": "Glenn S.",
         "Age": 20,
         "Gender": "Male",
         "Phone": "1472583690",
-        "CourseID": 0
+        "CourseID": 0,
+        "Result": {
+            0: 75,
+            1: 60,
+            2: 77
+        }
     }
-]
+}
 
-classes = [
-    {
-        "id": 0,
-        "Name": "Python"
+classes = {
+    0: {
+        "Name": "Python",
+        "Max": 100
     },
-    {
-        "id": 1,
-        "Name": "C# / .NET"
+    1: {
+        "Name": "C# / .NET",
+        "Max": 100
     },
-    {
-        "id": 2,
-        "Name": "Frameworks"
+    2: {
+        "Name": "Frameworks",
+        "Max": 100
     },
-    {
-        "id": 3,
-        "Name": "HTML5"
+    3: {
+        "Name": "HTML5",
+        "Max": 100
     },
-    {
-        "id": 4,
-        "Name": "PHP"
+    4: {
+        "Name": "PHP",
+        "Max": 100
     },
-    {
-        "id": 5,
-        "Name": "CSS"
+    5: {
+        "Name": "CSS",
+        "Max": 100
     }
-]
+}
 
-courses = [
-    {
-        "id": 0,
+courses = {
+     0: {
         "Name": "Junior Python Developer",
         "ClassesID": [0, 1, 2]
     },
-    {
-        "id": 1,
+    1: {
         "Name": "Full-Stack Webdeveloper",
         "ClassesID": [3, 4, 5]
     }
-]
+}
+
+################## Main Route ##################
 
 @app.route('/')
 def homeScreen():
@@ -96,20 +108,18 @@ def getCourses(id):
 @app.route('/courses/<int:id>/students', methods=['GET'])
 def getCoursesWithStudents(id):
     resultList = []
-    for student in students:
-        if student["CourseID"] == id:
-            resultList.append(student)
+    for key in range(len(students)):
+        if(students[key]["CourseID"] == id):
+            resultList.append(students[key])
     return jsonify(resultList)
 
 @app.route('/courses/<int:id>/classes', methods=['GET'])
 def getCoursesWithClasses(id):
     resultList = []
-    for course in courses:
-        if course["ClassesID"] == id:
-            classID = course["ClassesID"]
-            for i in classID:
-                resultList.append(classes[i])
-
+    for item in courses[id]["ClassesID"]:
+        resultList.append(classes[item])
+    return jsonify(resultList)
+    
 ################## Classes ##################
 
 @app.route('/classes')
@@ -120,6 +130,26 @@ def showClasses():
 def getClasses(id):
     return jsonify(classes[id])
 
+@app.route('/classes/<int:id>/students', methods=['GET'])
+def getClassesWithStudents(id):
+    resultlist = []
+    for courseID in courses:
+        if id in courses[courseID]["ClassesID"]:
+            for studentID in students:
+                if(students[studentID]["CourseID"] == courseID):
+                    resultlist.append(students[studentID])
+    return jsonify(resultlist)
+
+
+
+@app.route('/classes/<int:id>/courses', methods=['GET'])
+def getClassesWithCourses(id):
+    resultlist = []
+    for courseID in courses:
+        if id in courses[courseID]["ClassesID"]:
+            resultlist.append(courses[courseID])
+    return jsonify(resultlist)
+
 ################## students ##################
 
 @app.route('/students')
@@ -129,6 +159,17 @@ def showStudents():
 @app.route('/students/<int:id>', methods=['GET'])
 def getStudents(id):
     return jsonify(students[id])
+
+@app.route('/students/<int:id>/results', methods=['GET'])
+def getStudentsWithResults(id):
+    resultList = []
+    results = students[id]["Result"]
+    for resultID in results:
+        nameClasses = classes[resultID]["Name"]
+        maxResult = classes[resultID]["Max"]
+        score = results[resultID]
+        resultList.append(f'Score for: {nameClasses} = {score}/{maxResult}')
+    return jsonify(resultList)
 
 ################## Run The App ##################
 
